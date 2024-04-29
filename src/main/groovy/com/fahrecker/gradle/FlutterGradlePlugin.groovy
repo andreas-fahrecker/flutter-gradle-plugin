7a685fd6210f80c2db15f31606ec86de8da86fc0
Andreas Fahrecker <andreas@fahrecker.com>
2024-04-29 08:12:06 +0200

    refactor: extract FlutterExtension into own file

--- src/main/groovy/com/fahrecker/gradle/FlutterGradlePlugin.groovy
+++ src/main/groovy/com/fahrecker/gradle/FlutterGradlePlugin.groovy
@@ -41,27 +41,3 @@ class FlutterGradlePlugin implements Plugin<Project> {
         }
     }
 }
-
-class FlutterExtension {
-    final Property<String> flutterVersion
-    final Property<String> flutterChannel
-    final Property<String> flutterSdkPath
-
-    FlutterExtension(Project project) {
-        flutterVersion = project.objects.property(String)
-        flutterChannel = project.objects.property(String)
-        flutterSdkPath = project.objects.property(String)
-    }
-
-    void setFlutterVersion(String version) {
-        flutterVersion.set(version)
-    }
-
-    void setFlutterChannel(String channel) {
-        flutterChannel.set(channel)
-    }
-
-    void setFlutterSdkPath(String path) {
-        flutterSdkPath.set(path)
-    }
-}
